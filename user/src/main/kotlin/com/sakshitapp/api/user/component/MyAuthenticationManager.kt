7b2a5dbb86259065efd4ca457d10131577758e5f
Punit Chhajer <punitchhajer@hotmail.com>
2021-03-21 13:50:58 +0530

    Change fix build error for user

--- user/src/main/kotlin/com/sakshitapp/api/user/component/MyAuthenticationManager.kt
+++ user/src/main/kotlin/com/sakshitapp/api/user/component/MyAuthenticationManager.kt
@@ -2,8 +2,8 @@ package com.sakshitapp.api.user.component
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseToken
-import com.sakshitapp.api.base.model.User
 import com.sakshitapp.api.user.service.UserService
+import com.sakshitapp.shared.model.User
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.authentication.ReactiveAuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
@@ -26,8 +26,16 @@ class MyAuthenticationManager : ReactiveAuthenticationManager {
             val token = firebaseAuth.verifyIdToken(authToken)
             if (token != null) {
                 val user = firebaseTokenToUserDto(token)
-                return userService.get(user.uid).switchIfEmpty(userService.save(user))
-                        .flatMap { Mono.just(UsernamePasswordAuthenticationToken(it.uid, token, it.getGrandAuthorities())) }
+                return userService.get(user.uuid).switchIfEmpty(userService.save(user))
+                    .flatMap {
+                        Mono.just(
+                            UsernamePasswordAuthenticationToken(
+                                it.uid,
+                                token,
+                                it.getGrandAuthorities()
+                            )
+                        )
+                    }
             }
         } catch (e: Exception) {
             return Mono.empty()
@@ -36,10 +44,10 @@ class MyAuthenticationManager : ReactiveAuthenticationManager {
     }
 
     private fun firebaseTokenToUserDto(decodedToken: FirebaseToken): User = User(
-            uid = decodedToken.uid,
-            name = decodedToken.name,
-            photoURL = decodedToken.picture,
-            email = decodedToken.email,
-            phoneNumber = decodedToken.tenantId
+        uuid = decodedToken.uid,
+        name = decodedToken.name,
+        photoURL = decodedToken.picture,
+        email = decodedToken.email,
+        phoneNumber = decodedToken.tenantId
     )
 }
