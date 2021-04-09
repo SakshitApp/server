6a0085acfddb3c90c172840f6e8fa924f5c64634
Punit Chhajer <punitchhajer@hotmail.com>
2021-04-09 19:56:30 +0530

    get subscribed course detail

--- course/src/main/kotlin/com/sakshitapp/api/course/service/CourseService.kt
+++ course/src/main/kotlin/com/sakshitapp/api/course/service/CourseService.kt
@@ -19,64 +19,68 @@ class CourseService {
 
     fun getHome(user: User): Mono<Home> {
         val c: Mono<List<Course>> = courseRepository.findAllByState(CourseState.ACTIVE.name)
-            .collectList()
-            .subscribeOn(Schedulers.parallel())
-        val item: Mono<MutableList<Subscription>> =
-            subscriptionRepository.findAllByUser(user.uid)
                 .collectList()
                 .subscribeOn(Schedulers.parallel())
+        val item: Mono<MutableList<Subscription>> =
+                subscriptionRepository.findAllByUser(user.uid)
+                        .collectList()
+                        .subscribeOn(Schedulers.parallel())
 
         return Mono.zip(c, item)
-            .flatMap { Mono.just(Home(subscribed = it.t2, courses = it.t1)) }
+                .flatMap { touple ->
+                    Mono.just(Home(subscribed = touple.t2.map { subs ->
+                        subs.copy(course = touple.t1.findLast { it.uuid == subs.courseId })
+                    }, courses = touple.t1))
+                }
     }
 
     fun get(user: User, course: String): Mono<Subscription> =
-        courseRepository.findById(course)
-            .flatMap { course ->
-                subscriptionRepository.findById(user.uid, course.uuid)
-                    .switchIfEmpty(
-                        subscriptionRepository.save(
-                            Subscription(
-                                user = user.uid,
-                                courseId = course.uuid
-                            )
-                        )
-                    ).map { it.copy(course = course) }
-            }
+            courseRepository.findById(course)
+                    .flatMap { course ->
+                        subscriptionRepository.findById(user.uid, course.uuid)
+                                .switchIfEmpty(
+                                        subscriptionRepository.save(
+                                                Subscription(
+                                                        user = user.uid,
+                                                        courseId = course.uuid
+                                                )
+                                        )
+                                ).map { it.copy(course = course) }
+                    }
 
     fun like(user: User, course: String): Mono<Subscription> =
-        subscriptionRepository.findById(user.uid, course)
-            .flatMap { subscription ->
-                if (subscription.isLiked) {
-                    courseRepository.findById(course)
-                        .flatMap { Mono.just(subscription.copy(course = it)) }
-                } else {
-                    courseRepository.findById(course)
-                        .flatMap { courseRepository.save(it.copy(likes = it.likes + 1)) }
-                        .flatMap {
-                            subscriptionRepository.save(
-                                subscription.copy(
-                                    isLiked = true
-                                )
-                            )
+            subscriptionRepository.findById(user.uid, course)
+                    .flatMap { subscription ->
+                        if (subscription.isLiked) {
+                            courseRepository.findById(course)
+                                    .flatMap { Mono.just(subscription.copy(course = it)) }
+                        } else {
+                            courseRepository.findById(course)
+                                    .flatMap { courseRepository.save(it.copy(likes = it.likes + 1)) }
+                                    .flatMap {
+                                        subscriptionRepository.save(
+                                                subscription.copy(
+                                                        isLiked = true
+                                                )
+                                        )
+                                    }
                         }
-                }
-            }
+                    }
 
     fun review(user: User, course: String, review: String): Mono<Course> =
-        courseRepository.findById(course)
-            .flatMap {
-                val list = arrayListOf<Review>()
-                list.addAll(it.review)
-                list.add(
-                    Review(
-                        user = user.uid,
-                        userImage = user.photoURL,
-                        userName = user.name,
-                        review = review
-                    )
-                )
-                courseRepository.save(it.copy(review = list))
-            }
+            courseRepository.findById(course)
+                    .flatMap {
+                        val list = arrayListOf<Review>()
+                        list.addAll(it.review)
+                        list.add(
+                                Review(
+                                        user = user.uid,
+                                        userImage = user.photoURL,
+                                        userName = user.name,
+                                        review = review
+                                )
+                        )
+                        courseRepository.save(it.copy(review = list))
+                    }
 
 }
